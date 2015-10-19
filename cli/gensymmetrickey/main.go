61c1cfd926c90dd9211d4f42548f64b68073bed5
Frank Xu <frank.xu@northbright.com>
2015-10-19 12:21:24 +0800

    Add cli: gensymmetrickey

---
+++ cli/gensymmetrickey/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/northbright/keygen"
+	"os"
+)
+
+const (
+	defaultKeySize    int         = 512
+	defaultOutputFile string      = "./key.dat"
+	defaultPerm       os.FileMode = 0666
+)
+
+var (
+	keySize    int    // Key Size
+	outputFile string // Output Key File
+)
+
+func main() {
+
+	flag.StringVar(&outputFile, "o", defaultOutputFile, "Output key file. Ex: -a='./mykey.dat'. Default: ./key.dat")
+	flag.IntVar(&keySize, "b", defaultKeySize, "Key Size(bits). Ex: -b='256'. Default: 512 bits")
+
+	flag.Parse()
+
+	if err := keygen.GenSymmetricKeyFile(keySize, outputFile, defaultPerm); err != nil {
+		fmt.Printf("Generate symmetric key error: %v\n", err)
+	} else {
+		fmt.Printf("Generate symmetric key successfully.\n")
+	}
+}
